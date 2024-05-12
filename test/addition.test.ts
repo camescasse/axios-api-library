3ba693aaabeae8ccdd678bd6aa85556c8803df2e
camescasse <e.camescasse@gmail.com>
2024-05-12 12:36:48 -0400

    Setup Vitest

--- test/addition.test.ts
+++ test/addition.test.ts
@@ -1,10 +1,8 @@
-import { expect } from 'chai';
+import { describe, it, expect } from 'vitest';
 
 describe('Test Group', () => {
   it('should return 4 when given 2 + 2', () => {
-    function addition(a, b) {
-      return a + b;
-    }
+    const addition = (first: number, second: number) => first + second;
     const result = addition(2, 2);
 
     expect(result).equals(4);
