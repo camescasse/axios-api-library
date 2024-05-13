24c78abd769113314091bcb68203c9f87ebc0c3a
camescasse <e.camescasse@gmail.com>
2024-05-13 15:31:09 -0400

    Fix AuthorService endpoint

--- src/services/AuthorService.ts
+++ src/services/AuthorService.ts
@@ -1,7 +1,7 @@
 import Author from '../models/Author';
 import APIClient from './APIClient';
 
-const ENDPOINT = '/api/authors';
+const ENDPOINT = '/api/authors/';
 
 const client = new APIClient<Author>(ENDPOINT);
 
